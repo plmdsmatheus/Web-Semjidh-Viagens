016b3f893304f2751c67ef9dda2bb647cb065db3
Matheus Duarte <84824460+plmdsmatheus@users.noreply.github.com>
2025-09-17 15:41:28 +0000

    Fix: models.py

--- semjidhapp/viagens/models.py
+++ semjidhapp/viagens/models.py
@@ -8,7 +8,7 @@ class Carro(models.Model):
     placa = models.CharField(max_length=7)
 
     def __str__(self):
-        return self.marca + " " + self.modelo + "-" + self.placa
+        return f"{self.marca} \033[1m{self.modelo} - {self.placa}\033[0m"
 
 class Motorista(models.Model):
     nome = models.CharField(max_length=100)
