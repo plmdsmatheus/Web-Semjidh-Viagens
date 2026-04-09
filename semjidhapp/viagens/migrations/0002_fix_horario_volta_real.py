9da820a14f93b544961c2a2fcb5832ad4d42247a
Matheus Duarte <84824460+plmdsmatheus@users.noreply.github.com>
2026-04-09 13:23:31 +0000

    FIx: Typo in migrations

--- semjidhapp/viagens/migrations/0002_fix_horario_volta_real.py
+++ semjidhapp/viagens/migrations/0002_fix_horario_volta_real.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('viagens', '0001_inital.py'),
+        ('viagens', '0001_inital'),
     ]
 
     operations = [
